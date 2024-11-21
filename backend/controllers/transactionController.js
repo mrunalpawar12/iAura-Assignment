7c6f483a2d5c58137cfb23053965b659035199b7
mrunal pawar <m.pawar1203@gmail.com>
2024-11-22 03:31:43 +0530

    first commit

---
+++ backend/controllers/transactionController.js
@@ -0,0 +1,193 @@
+import Transaction from "../models/TransactionModel.js";
+import User from "../models/UserSchema.js";
+import moment from "moment";
+
+export const addTransactionController = async (req, res) => {
+  try {
+    const {
+      title,
+      amount,
+      description,
+      date,
+      category,
+      userId,
+      transactionType,
+    } = req.body;
+
+    // console.log(title, amount, description, date, category, userId, transactionType);
+
+    if (
+      !title ||
+      !amount ||
+      !description ||
+      !date ||
+      !category ||
+      !transactionType
+    ) {
+      return res.status(408).json({
+        success: false,
+        messages: "Please Fill all fields",
+      });
+    }
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    let newTransaction = await Transaction.create({
+      title: title,
+      amount: amount,
+      category: category,
+      description: description,
+      date: date,
+      user: userId,
+      transactionType: transactionType,
+    });
+
+    user.transactions.push(newTransaction);
+
+    user.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Transaction Added Successfully",
+    });
+  } catch (err) {
+    return res.status(401).json({
+      success: false,
+      messages: err.message,
+    });
+  }
+};
+
+export const getAllTransactionController = async (req, res) => {
+  try {
+    const { type, frequency, startDate, endDate } = req.body;
+
+    // Create the query object for fetching all transactions
+    const query = {};
+
+    // Apply the 'type' filter if provided
+    if (type && type !== 'all') {
+      query.transactionType = type;
+    }
+
+    // Apply the 'frequency' filter if provided (Last week, last month, etc.)
+    if (frequency && frequency !== 'custom') {
+      query.date = {
+        $gt: moment().subtract(Number(frequency), "days").toDate()
+      };
+    } 
+    // Apply custom date range filter if 'startDate' and 'endDate' are provided
+    else if (startDate && endDate) {
+      query.date = {
+        $gte: moment(startDate).toDate(),
+        $lte: moment(endDate).toDate(),
+      };
+    }
+
+    // Fetch transactions based on the constructed query
+    const transactions = await Transaction.find(query);
+
+    return res.status(200).json({
+      success: true,
+      transactions: transactions,
+    });
+  } catch (err) {
+    return res.status(401).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
+
+
+export const deleteTransactionController = async (req, res) => {
+  try {
+    const transactionId = req.params.id;
+
+    // Find and delete the transaction by its ID
+    const transactionElement = await Transaction.findByIdAndDelete(transactionId);
+
+    if (!transactionElement) {
+      return res.status(400).json({
+        success: false,
+        message: "Transaction not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: `Transaction successfully deleted`,
+    });
+  } catch (err) {
+    return res.status(401).json({
+      success: false,
+      messages: err.message,
+    });
+  }
+};
+
+export const updateTransactionController = async (req, res) => {
+  try {
+    const transactionId = req.params.id;
+
+    const { title, amount, description, date, category, transactionType } =
+      req.body;
+
+    console.log(title, amount, description, date, category, transactionType);
+
+    const transactionElement = await Transaction.findById(transactionId);
+
+    if (!transactionElement) {
+      return res.status(400).json({
+        success: false,
+        message: "transaction not found",
+      });
+    }
+
+    if (title) {
+      transactionElement.title = title;
+    }
+
+    if (description) {
+      transactionElement.description = description;
+    }
+
+    if (amount) {
+      transactionElement.amount = amount;
+    }
+
+    if (category) {
+      transactionElement.category = category;
+    }
+    if (transactionType) {
+      transactionElement.transactionType = transactionType;
+    }
+
+    if (date) {
+      transactionElement.date = date;
+    }
+
+    await transactionElement.save();
+
+    // await transactionElement.remove();
+
+    return res.status(200).json({
+      success: true,
+      message: `Transaction Updated Successfully`,
+      transaction: transactionElement,
+    });
+  } catch (err) {
+    return res.status(401).json({
+      success: false,
+      messages: err.message,
+    });
+  }
+};
---
+++ frontend/src/components/Particle.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css";
+
+
+const Particles = () => {
+  return (
+    <div className="w-full h-screen simple-background">
+      {/* Content goes here */}
+    </div>
+  );
+};
+
+export default Particles;
